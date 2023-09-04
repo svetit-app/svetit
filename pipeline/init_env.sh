f763032deca7d39e2804dac7797421b23054b0e4
prt0117 <cpd900@yandex.ru>
2023-09-04 16:29:23 +0600

    setting secret for realm import during init

--- pipeline/init_env.sh
+++ pipeline/init_env.sh
@@ -17,7 +17,12 @@ cp "$SCRIPT_PATH"/.env{.example,}
 
 PASS_KEYS="DB_PASS SSO_DB_PASS SSO_ADMIN_PASS AUTH_DB_PASS APP_DB_PASS OIDC_CLIENT_SECRET"
 for i in $PASS_KEYS; do
-	sed -i "s/{{$i}}/$(genPassword)/" "$SCRIPT_PATH/.env"
+	password=$(genPassword)
+	sed -i "s/{{$i}}/$password/" "$SCRIPT_PATH/.env"
+	if [ $i = "OIDC_CLIENT_SECRET" ]; then
+		sed -i "s/\"secret\":.*/\"secret\": \"$password\",/g" "$SCRIPT_PATH/keycloak/import/realm-export.json"
+	fi
 done
+
 
 echo "New ENV file generated"
