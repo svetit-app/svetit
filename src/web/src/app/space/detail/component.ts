9300ee7629a39bafc7bbf3dc72169037b8193764
prt0117 <cpd900@yandex.ru>
2023-09-25 16:56:27 +0600

    ngFor for mocked data

--- src/web/src/app/space/detail/component.ts
+++ src/web/src/app/space/detail/component.ts
@@ -1,12 +1,46 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
+interface Invite {
+	user: string;
+	roles: string[];
+}
+
+interface Link {
+	name: string;
+	date: string;	
+}
+
+interface User {
+	name: string;
+	login: string;
+	roles: string[];
+}
+
 @Component({
 	selector: 'app-temp',
 	templateUrl: './component.html',
 	styleUrls: ['./component.css']
 })
 export class SpaceDetailComponent implements OnInit {
+
+	invites: Invite[] = [
+		{user: "petya", roles: ["admin", "user", "guest"]},
+		{user: "vasya", roles: ["admin", "user", "guest"]},
+		{user: "kolya", roles: ["admin", "user", "guest"]},
+	];
+
+	links: Link[] = [
+		{name: "ref21322", date: "19.12.23"},
+		{name: "ref98123", date: "10.07.24"},
+		{name: "ref74332", date: "04.03.25"},
+	];
+
+	users: User[] = [
+		{name: "Петр Петрович", login: "petya", roles: ["admin", "user", "guest"]},
+		{name: "Василий Иванович", login: "vasya", roles: ["admin", "user", "guest"]},
+		{name: "Николай Александрович", login: "kolya", roles: ["admin", "user", "guest"]},
+	];
 
 	constructor(
 		private route: ActivatedRoute,
--- src/web/src/app/space/list/component.ts
+++ src/web/src/app/space/list/component.ts
@@ -1,5 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {NgFor} from '@angular/common';
+
+interface SpaceInvite {
+	name: string;
+}
+
+interface SpaceInviteMine {
+	name: string;
+	user: string;
+	roles: string[];
+}
+
+interface SpaceRef {
+	space: string;
+	name: string;
+	date: string;
+}
+
+interface SpacesList {
+	name: string;
+	key: string;
+}
 
 @Component({
 	selector: 'app-temp',
@@ -7,6 +29,30 @@ import {ActivatedRoute} from '@angular/router';
 	styleUrls: ['./component.css']
 })
 export class SpaceListComponent implements OnInit {
+
+	spaces: SpaceInvite[] = [
+		{name: 'Пространство №1'},
+		{name: 'Пространство №2'},
+		{name: 'Пространство №3'},
+	];
+
+	spacesmine: SpaceInviteMine[] = [
+		{name: 'Пространство №1', user: "vasya", roles: ["admin", "user", "guest"]},
+		{name: 'Пространство №2', user: "petya", roles: ["admin", "user", "guest"]},
+		{name: 'Пространство №3', user: "kolya", roles: ["admin", "user", "guest"]},
+	];
+
+	spacerefs: SpaceRef[] = [
+		{space: 'Пространство №1', name: "ref123", date: "19.04.2024"},
+		{space: 'Пространство №2', name: "ref456", date: "27.05.2025"},
+		{space: 'Пространство №3', name: "ref789", date: "03.10.2023"},
+	];
+
+	spaceslist: SpacesList[] = [
+		{name: 'Пространство №1', key: 'key13219'},
+		{name: 'Пространство №2', key: 'key41983'},
+		{name: 'Пространство №3', key: 'key92333'},
+	];
 
 	constructor(
 		private route: ActivatedRoute,
