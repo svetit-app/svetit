935211b8a5069310916fea98567fa33ec1e57a81
prt0117 <cpd900@yandex.ru>
2023-09-27 17:09:00 +0600

    WIP autocomplete at space add page

--- src/web/src/app/space/add/component.ts
+++ src/web/src/app/space/add/component.ts
@@ -3,17 +3,36 @@ import {ActivatedRoute} from '@angular/router';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {startWith, map} from 'rxjs/operators';
+import { MatOption } from '@angular/material/core';
 
 @Component({
 	selector: 'app-space-add',
 	templateUrl: './component.html',
 	styleUrls: ['./component.css']
 })
+
 export class SpaceAddComponent implements OnInit {
 
 	control = new FormControl('');
-  	spaces: string[] = ['Пространство №1', 'Пространство №2', 'Пространство №3', 'Пространство №4'];
+  	spaces: string[] = [
+		'Пространство №1 ABC',
+		'Пространство №2 DEF',
+		'Пространство №3 GHI',
+		'Пространство №4 JKL',
+		'Пространство №5 MNO',
+		'Пространство №6 PQR',
+		'Пространство №7 STU',
+		'Пространство №8 VWX',
+		'Пространство №9 YZA',
+		'Пространство №10 BCD',
+		'Пространство №11 EFG',
+		'Пространство №12 HIJ',
+		'Пространство №13 KLM',
+		'Пространство №14 NOP',
+		'Пространство №15 QRS',
+	];
   	filteredSpaces: Observable<string[]>;
+	selectedSpace: string;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -26,13 +45,21 @@ export class SpaceAddComponent implements OnInit {
 			map(value => this._filter(value || '')),
 		);
 	}
-
 	private _filter(value: string): string[] {
 		const filterValue = this._normalizeValue(value);
-		return this.spaces.filter(space => this._normalizeValue(space).includes(filterValue));
+		return this.spaces.filter(space => this._normalizeValue(space).includes(filterValue)).slice(0,10);
 	}
 	
 	private _normalizeValue(value: string): string {
 		return value.toLowerCase().replace(/\s/g, '');
+	}
+
+	OnSelected(option: MatOption) {
+		if (option){
+			this.selectedSpace = option.value;
+		} else {
+			this.selectedSpace = null;
+		}
+		
 	}
 }
