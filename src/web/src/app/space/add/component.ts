9dabd414d0152028b962755eb6cd6637c4046baa
prt0117 <cpd900@yandex.ru>
2023-10-02 16:44:43 +0600

    autocmplt to watch changes in spaces, new sbscrb

--- src/web/src/app/space/add/component.ts
+++ src/web/src/app/space/add/component.ts
@@ -1,16 +1,11 @@
-import {Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {FormControl} from '@angular/forms';
-import {Observable, Subject} from 'rxjs';
-import {startWith, map} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { Observable, Subject, of } from 'rxjs';
+import { startWith, map, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { MatOption } from '@angular/material/core';
-import { debounceTime } from 'rxjs/operators';
-import { distinctUntilChanged } from 'rxjs/operators';
-import { takeUntil } from 'rxjs/operators';
-import { Router} from '@angular/router';
-import { FormGroup } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 interface Space {
 	name: string;
@@ -22,27 +17,14 @@ interface Space {
 	templateUrl: './component.html',
 	styleUrls: ['./component.css'],
 })
+
 export class SpaceAddComponent implements OnInit {
-
 	createForm: FormGroup;
-
 	controlAutocomplete = new FormControl('');
-
-  	spaces: string[] = [];
-
 	spacesWithKeys: Space[] = [];
-
-	keys: string[] = [
-		'key12345',
-		'key67890'
-	];
-
-  	filteredSpaces: Observable<string[]>;
-
-	selectedSpace: string;
-
+	filteredSpaces: Observable<Space[]>;
+	selectedSpace: Space;
 	private readonly ngUnsubscribe = new Subject();
-
 	keyWasChanged: boolean = false;
 
 	constructor(
@@ -53,7 +35,15 @@ export class SpaceAddComponent implements OnInit {
 		this._createForm();
 	}
 
-	private translitFromRuToEn = {"Ё":"YO","Й":"I","Ц":"TS","У":"U","К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH","З":"Z","Х":"H","Ъ":"'","ё":"yo","й":"i","ц":"ts","у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh","щ":"sch","з":"z","х":"h","ъ":"'","Ф":"F","Ы":"I","В":"V","А":"A","П":"P","Р":"R","О":"O","Л":"L","Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v","а":"a","п":"p","р":"r","о":"o","л":"l","д":"d","ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M","И":"I","Т":"T","Ь":"'","Б":"B","Ю":"YU","я":"ya","ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"'","б":"b","ю":"yu"};
+	private translitFromRuToEn = {
+	"Ё": "YO", "Й": "I", "Ц": "TS", "У": "U", "К": "K", "Е": "E", "Н": "N", "Г": "G", "Ш": "SH", "Щ": "SCH",
+	"З": "Z", "Х": "H", "Ъ": "'", "ё": "yo", "й": "i", "ц": "ts", "у": "u", "к": "k", "е": "e", "н": "n", "г": "g",
+	"ш": "sh", "щ": "sch", "з": "z", "х": "h", "ъ": "'", "Ф": "F", "Ы": "I", "В": "V", "А": "A", "П": "P", "Р": "R",
+	"О": "O", "Л": "L", "Д": "D", "Ж": "ZH", "Э": "E", "ф": "f", "ы": "i", "в": "v", "а": "a", "п": "p", "р": "r",
+	"о": "o", "л": "l", "д": "d", "ж": "zh", "э": "e", "Я": "Ya", "Ч": "CH", "С": "S", "М": "M", "И": "I", "Т": "T",
+	"Ь": "'", "Б": "B", "Ю": "YU", "я": "ya", "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "'", "б": "b",
+	"ю": "yu"
+	};
 
 	ngOnInit() {
 		this.filteredSpaces = this.controlAutocomplete.valueChanges.pipe(
@@ -65,70 +55,76 @@ export class SpaceAddComponent implements OnInit {
 			debounceTime(500),
 			distinctUntilChanged(),
 			takeUntil(this.ngUnsubscribe)
-		)
-		.subscribe(selected => {
+		).subscribe(selected => {
 			if (!selected) {
-				this.selectedSpace = "";
+			this.selectedSpace = null;
 			}
 		});
 	}
 
-	private _filter(value: string): string[] {
+	private _filter(value: string): Space[] {
 		const filterValue = this._normalizeValue(value);
-		return this.spaces.filter(space => this._normalizeValue(space).includes(filterValue)).slice(0,10);
+		return this.spacesWithKeys.filter(space => this._normalizeValue(space.name).includes(filterValue)).slice(0, 10);
 	}
-	
+
 	private _normalizeValue(value: string): string {
 		return value.toLowerCase().replace(/\s/g, '');
 	}
 
 	onSelectOption(option: MatOption) {
-		if (option){
+		if (option) {
 			this.selectedSpace = option.value;
-		}		
-	}
-
-	sendRequestToJoin(){
-		this.router.navigate(['space/add/request/' + this.selectedSpace]);
-	}
-
-	private _transliterate(word: string): string{
-		let self = this;
-		return word.split('').map(function (char) { 
-		  return self.translitFromRuToEn[char] || char; 
-		}).join("");
-  	}
-
-	onNameChange(name: string) {
-		if (!this.keyWasChanged){
-			let newKey: string = this._transliterate(name);
-			newKey = newKey.replace(/[^a-zA-Z0-9_]/g, '');
-			this.createForm.patchValue({
-				key : newKey
-			   });
 		}
 	}
 
-	onKeyChange(key: string){
+	sendRequestToJoin() {
+		if (this.selectedSpace) {
+			this.router.navigate(['space/add/request/' + this.selectedSpace.key]);
+		}
+	}
+
+	private _transliterate(word: string): string {
+		let self = this;
+		return word.split('').map(function (char) {
+			return self.translitFromRuToEn[char] || char;
+		}).join("");
+	}
+
+	onNameChange(name: string) {
+		if (!this.keyWasChanged) {
+			let newKey: string = this._transliterate(name);
+			newKey = newKey.replace(/[^a-zA-Z0-9_]/g, '');
+			this.createForm.patchValue({
+				key: newKey
+			});
+		}
+	}
+
+	onKeyChange(key: string) {
 		this.keyWasChanged = true;
 	}
 
 	onSubmitCreate(data): void {
-		if (this.createForm.invalid){
+		if (this.createForm.invalid) {
 			console.log("form is invalid");
 			return;
 		}
 		let contains: boolean = this.spacesWithKeys.some(e => e.key === data.key);
+
 		if (!contains) {
 			let spaceWithKey: Space = {
 				name: data.name,
 				key: data.key
 			};
 			this.spacesWithKeys.push(spaceWithKey);
-			this.spaces.push(spaceWithKey.name);
+			const filterValue = this._normalizeValue(this.controlAutocomplete.value || '');
+			const filtered = this.spacesWithKeys.filter(space =>
+				this._normalizeValue(space.name).includes(filterValue)
+			).slice(0, 10);
+			this.filteredSpaces = of(filtered);
 			console.log("space with key added");
 		} else {
-			this.createForm.controls['key'].setErrors({'incorrect': true});
+			this.createForm.controls['key'].setErrors({ 'incorrect': true });
 			console.log("space with such key exists");
 		}
 	}
