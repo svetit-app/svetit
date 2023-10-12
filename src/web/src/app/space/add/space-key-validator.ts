e47503ba2b3cdb889ce118638b53ded3e0c6eab4
prt0117 <cpd900@yandex.ru>
2023-10-12 12:12:45 +0600

    code style fixes

--- src/web/src/app/space/add/space-key-validator.ts
+++ src/web/src/app/space/add/space-key-validator.ts
@@ -6,21 +6,22 @@ import { SpaceService } from '../service';
 @Injectable({
 	providedIn: 'root',
 })
+
 export class SpaceKeyValidation implements AsyncValidator {
 
-constructor(private space: SpaceService) {}
+	constructor(private space: SpaceService) {}
 
 	validate = (control: AbstractControl) => {
 
-	const { value } = control;
+		const { value } = control;
 
-	return this.space.isExists(value)
-		.pipe(
-			map(res => {
-				if (res) {
-					return { 'keyExists': true};
-				}
-			}),
-		);
-  };
+		return this.space.isExists(value)
+			.pipe(
+				map(res => {
+					if (res) {
+						return { 'keyExists': true};
+					}
+				}),
+			);
+  	};
 }
