bab1e4c6462c9d2e63f886c896e21c4be64d802b
prt0117 <134271128+prt0117@users.noreply.github.com>
2024-01-30 09:29:15 +0600

    Update src/back/space/tests/test_create_space.py
    Co-authored-by: Kirill Zimnikov <kirill@zimnikov.ru>

--- src/back/space/tests/test_create_space.py
+++ src/back/space/tests/test_create_space.py
@@ -1,319 +1,171 @@
 import pytest
 
+h={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
+json={
+	'id': '',
+	'name': 'test12345678901',
+	'key': '67123',
+	'requestsAllowed': False,
+	'createdAt': 0
+}
 
 # run by make test-space or make run-space
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_no_auth(service_client):
-    """No authorization X-User header"""
-    response = await service_client.post(
-        '/space',
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '67123',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 401
+	"""No authorization X-User header"""
+	res = await service_client.post('/space', json=json)
+	assert res.status == 401
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_valid(service_client):
-    """Valid space creation"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '67123',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 201
+	"""Valid space creation"""
+	res = await service_client.post('/space', json=json, headers=h)
+	assert res.status == 201
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_no_name(service_client):
-    """No name param in request json"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'key': '67123',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 500
+	"""No name param in request json"""
+	data = json.copy()
+	del data['name']
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 500
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_no_key(service_client):
-    """No key param in request json"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 500
+	"""No key param in request json"""
+	data = json.copy()
+	del data['key']
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 500
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_no_requests_allowed(service_client):
-    """No requestsAllowed param in request json"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '67123',
-            'createdAt': 0
-        }
-    )
-    assert response.status == 500
+	"""No requestsAllowed param in request json"""
+	data = json.copy()
+	del data['requestsAllowed']
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 500
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_key_is_uuid_but_not_as_x_user(service_client):
-    """key is uuid but not equals the caller uuid"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '00000000-a69a-4aec-b9c0-c80171d3ed86',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""key is uuid but not equals the caller uuid"""
+	data = json.copy()
+	data['key'] = '00000000-a69a-4aec-b9c0-c80171d3ed86'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_reserved_key_1(service_client):
-    """using reserved keyword key 1"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'u',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using reserved keyword key 1"""
+	data = json.copy()
+	data['key'] = 'u'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_reserved_key_2(service_client):
-    """using reserved keyword key 2"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'auth',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using reserved keyword key 2"""
+	data = json.copy()
+	data['key'] = 'auth'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_reserved_key_3(service_client):
-    """using reserved keyword key 3"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'settings',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using reserved keyword key 3"""
+	data = json.copy()
+	data['key'] = 'settings'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_reserved_key_4(service_client):
-    """using reserved keyword key 4"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'main',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using reserved keyword key 4"""
+	data = json.copy()
+	data['key'] = 'main'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_reserved_key_5(service_client):
-    """using reserved keyword key 5"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'api',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using reserved keyword key 5"""
+	data = json.copy()
+	data['key'] = 'api'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_regex_invalid_key_1(service_client):
-    """using invalid by regex key 1"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '-123d',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using invalid by regex key 1"""
+	data = json.copy()
+	data['key'] = '-123d'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_regex_invalid_key_2(service_client):
-    """using invalid by regex key 2"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': 'Б123d',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using invalid by regex key 2"""
+	data = json.copy()
+	data['key'] = 'Б123d'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_regex_invalid_key_3(service_client):
-    """using invalid by regex key 3"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using invalid by regex key 3"""
+	data = json.copy()
+	data['key'] = ''
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_invalid_key_too_long(service_client):
-    """using too long key"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '1234567890123456789012345678901234567',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 500
+	"""using too long key"""
+	data = json.copy()
+	data['key'] = '1234567890123456789012345678901234567'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 500
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_name_is_empty(service_client):
-    """using empty name"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': '',
-            'key': '1234567890',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 400
+	"""using empty name"""
+	data = json.copy()
+	data['name'] = ''
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 400
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_requests_allowed_is_not_bool(service_client):
-    """requestsAllowed is not bool in request json"""
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': '123',
-            'key': '1234567890',
-            'requestsAllowed': "False",
-            'createdAt': 0
-        }
-    )
-    assert response.status == 500
+	"""requestsAllowed is not bool in request json"""
+	data = json.copy()
+	data['requestsAllowed'] = 'False'
+	res = await service_client.post('/space', json=data, headers=h)
+	assert res.status == 500
 
 
 @pytest.mark.pgsql('V0001__Init', files=['test_data.sql'])
 async def test_create_space_duplicate_key(service_client):
-    """using duplicate key"""
-    # creating new space
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '67123',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 201
+	"""using duplicate key"""
+	# creating new space
+	res = await service_client.post('/space', json=json, headers=h)
+	assert res.status == 201
 
-    # then trying to create one more with same key
-    response = await service_client.post(
-        '/space',
-        headers={'X-User': 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'},
-        json={
-            'id': '',
-            'name': 'test12345678901',
-            'key': '67123',
-            'requestsAllowed': False,
-            'createdAt': 0
-        }
-    )
-    assert response.status == 409
+	# then trying to create one more with same key
+	res = await service_client.post('/space', json=json, headers=h)
+	assert res.status == 409
