82016f1d887383b5607da1c7a3c43a3757283ab3
Кирилл Зимников <kirill@zimnikov.ru>
2024-05-21 10:20:48 +0700

    100 добавил хидеры по умолчанию в тесты

--- src/back/project/tests/conftest.py
+++ src/back/project/tests/conftest.py
@@ -8,6 +8,33 @@ pytest_plugins = [
 ]
 
 
+@pytest.fixture
+def default_user_id():
+	return 'd2f9924d-a69a-4aec-b9c0-c80171d3ed86'
+
+
+@pytest.fixture
+def default_space_id():
+	return '11111111-1111-1111-1111-111111111111'
+
+
+@pytest.fixture
+def service_client_options(
+	service_client_options,
+	service_client_default_headers,
+	default_user_id,
+	default_space_id,
+):
+	opts = service_client_options
+	opts['headers'] = {
+		**service_client_default_headers,
+		'X-User': default_user_id,
+		'X-Space-Id': default_space_id,
+		'X-Space-Role': 'admin',
+	}
+	return opts
+
+
 @pytest.fixture(scope='session')
 def pgsql_local(service_source_dir, pgsql_local_create):
 	databases = discover.find_schemas(
