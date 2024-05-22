63112fbb3fce7c0884da795c0916e1cb65dd42ec
prt0117 <cpd900@yandex.ru>
2024-05-22 10:44:14 +0500

    added space_id column for every ms project tables

--- src/back/project/db/app/migrations/V0001__Init.sql
+++ src/back/project/db/app/migrations/V0001__Init.sql
@@ -29,6 +29,7 @@ COMMENT ON TYPE project.param_value_type IS 'Type of the param value.';
 
 CREATE TABLE project.param_type (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	parent_id BIGINT DEFAULT NULL REFERENCES project.param_type (id) ON DELETE CASCADE,
 	key VARCHAR(64) NOT NULL,
 	name VARCHAR(64) DEFAULT NULL,
@@ -45,6 +46,7 @@ COMMENT ON COLUMN project.param_type.description IS 'Text description of param t
 COMMENT ON COLUMN project.param_type.value_type IS 'Type of the param type value.';
 
 CREATE TABLE project.project_param (
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(project_id, param_id)
@@ -56,6 +58,7 @@ COMMENT ON COLUMN project.project_param.param_id IS 'Param type.';
 
 CREATE TABLE project.section (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	name VARCHAR(64) DEFAULT NULL
 );
@@ -66,6 +69,7 @@ COMMENT ON COLUMN project.section.project_id IS 'The project that section belong
 COMMENT ON COLUMN project.section.name IS 'Name for appearing in web interface';
 
 CREATE TABLE project.section_param (
+	space_id UUID NOT NULL,
 	section_id BIGINT NOT NULL REFERENCES project.section (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(section_id, param_id)
@@ -77,6 +81,7 @@ COMMENT ON COLUMN project.section_param.param_id IS 'Param type.';
 
 CREATE TABLE project.cc_type (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	key VARCHAR(64) NOT NULL,
 	name VARCHAR(64) DEFAULT NULL,
@@ -92,6 +97,7 @@ COMMENT ON COLUMN project.cc_type.description IS 'The text description of CC typ
 
 CREATE TABLE project.control_circuit (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	type_id BIGINT NOT NULL REFERENCES project.cc_type (id),
 	section_id BIGINT NOT NULL REFERENCES project.section (id) ON DELETE CASCADE,
 	name VARCHAR(64) DEFAULT NULL
@@ -105,6 +111,7 @@ COMMENT ON COLUMN project.control_circuit.name IS 'The name of the CC that appea
 
 CREATE TABLE project.plugin (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	name VARCHAR(64) DEFAULT NULL,
 	description TEXT NOT NULL,
@@ -120,6 +127,7 @@ COMMENT ON COLUMN project.plugin.key IS 'The key of the plugin for addressing it
 
 CREATE TABLE project.device (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	plugin_id BIGINT DEFAULT NULL REFERENCES project.plugin (id),
 	name VARCHAR(64) NOT NULL,
@@ -134,6 +142,7 @@ COMMENT ON COLUMN project.device.name IS 'The name of the device that appears in
 COMMENT ON COLUMN project.device.check_interval_msec IS 'Device polling interval.';
 
 CREATE TABLE project.device_plugin_param (
+	space_id UUID NOT NULL,
 	device_id BIGINT NOT NULL REFERENCES project.device (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(device_id, param_id)
@@ -145,6 +154,7 @@ COMMENT ON COLUMN project.device_plugin_param.param_id IS 'Param type.';
 
 CREATE TABLE project.code (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	repository_id UUID NOT NULL,
 	commit_hash TEXT NOT NULL
@@ -158,6 +168,7 @@ COMMENT ON COLUMN project.code.commit_hash IS 'Commit in repository.';
 
 CREATE TABLE project.measure (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	name VARCHAR(10) NOT NULL
 );
@@ -169,6 +180,7 @@ COMMENT ON COLUMN project.measure.name IS 'Name of measure for appearing in web 
 
 CREATE TABLE project.save_timer (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	interval_msec INT NOT NULL
 );
@@ -179,6 +191,7 @@ COMMENT ON COLUMN project.save_timer.project_id IS 'Project which timer belongs 
 COMMENT ON COLUMN project.save_timer.interval_msec IS 'Interval of timer in milliseconds';
 
 CREATE TABLE project.cc_type_param (
+	space_id UUID NOT NULL,
 	cc_type_id BIGINT NOT NULL REFERENCES project.cc_type (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(cc_type_id, param_id)
@@ -198,6 +211,7 @@ COMMENT ON TYPE project.di_mode IS 'Type of value of device item.';
 
 CREATE TABLE project.di_type (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	measure_id BIGINT DEFAULT NULL REFERENCES project.measure (id) ON DELETE CASCADE,
 	save_timer_id BIGINT DEFAULT NULL REFERENCES project.save_timer (id),
 	key VARCHAR(64) NOT NULL,
@@ -216,6 +230,7 @@ COMMENT ON COLUMN project.di_type.mode IS 'Type of value of DI type.';
 COMMENT ON COLUMN project.di_type.save_algorithm IS 'Save algorithm for storing DI value.';
 
 CREATE TABLE project.di_plugin_param (
+	space_id UUID NOT NULL,
 	di_type_id BIGINT NOT NULL REFERENCES project.di_type (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(di_type_id, param_id)
@@ -226,6 +241,7 @@ COMMENT ON COLUMN project.di_plugin_param.di_type_id IS 'DI type which param typ
 COMMENT ON COLUMN project.di_plugin_param.param_id IS 'Param type.';
 
 CREATE TABLE project.cc_type_di_type (
+	space_id UUID NOT NULL,
 	cc_type_id BIGINT NOT NULL REFERENCES project.cc_type (id) ON DELETE CASCADE,
 	di_type_id BIGINT NOT NULL REFERENCES project.di_type (id),
 	PRIMARY KEY(cc_type_id, di_type_id)
@@ -237,6 +253,7 @@ COMMENT ON COLUMN project.cc_type_di_type.di_type_id IS 'DI type.';
 
 CREATE TABLE project.device_item (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	device_id BIGINT NOT NULL REFERENCES project.device (id) ON DELETE CASCADE,
 	type_id BIGINT NOT NULL REFERENCES project.di_type (id),
 	name VARCHAR(64) NOT NULL
@@ -250,6 +267,7 @@ COMMENT ON COLUMN project.device_item.name IS 'Name of DI';
 
 CREATE TABLE project.cc_mode_type (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	cc_type_id BIGINT DEFAULT NULL REFERENCES project.cc_type (id) ON DELETE CASCADE,
 	key VARCHAR(64) NOT NULL,
 	name VARCHAR(64) NOT NULL
@@ -262,6 +280,7 @@ COMMENT ON COLUMN project.cc_mode_type.key IS 'Key of mode for addressing.';
 COMMENT ON COLUMN project.cc_mode_type.name IS 'Name of mode that appears in web interface.';
 
 CREATE TABLE project.cc_di (
+	space_id UUID NOT NULL,
 	cc_id BIGINT NOT NULL REFERENCES project.control_circuit (id) ON DELETE CASCADE,
 	di_id BIGINT NOT NULL REFERENCES project.device_item (id),
 	PRIMARY KEY(cc_id, di_id)
@@ -272,6 +291,7 @@ COMMENT ON COLUMN project.cc_di.cc_id IS 'CC which DI belongs to.';
 COMMENT ON COLUMN project.cc_di.di_id IS 'DI.';
 
 CREATE TABLE project.cc_param (
+	space_id UUID NOT NULL,
 	cc_id BIGINT NOT NULL REFERENCES project.control_circuit (id) ON DELETE CASCADE,
 	param_id BIGINT NOT NULL REFERENCES project.param_type (id),
 	PRIMARY KEY(cc_id, param_id)
@@ -283,6 +303,7 @@ COMMENT ON COLUMN project.cc_param.param_id IS 'Param type.';
 
 CREATE TABLE project.cc_status_category (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	key VARCHAR(64) NOT NULL,
 	name VARCHAR(64) DEFAULT NULL,
@@ -298,6 +319,7 @@ COMMENT ON COLUMN project.cc_status_category.color IS 'Color of the status categ
 
 CREATE TABLE project.cc_status_type (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	cc_type_id BIGINT NOT NULL REFERENCES project.cc_type (id) ON DELETE CASCADE,
 	category_id BIGINT NOT NULL REFERENCES project.cc_status_category (id),
 	key VARCHAR(64) NOT NULL,
@@ -315,6 +337,7 @@ COMMENT ON COLUMN project.cc_status_type.inform IS 'Is it needed to inform user 
 
 CREATE TABLE project.value_view (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	di_type_id BIGINT NOT NULL REFERENCES project.di_type (id) ON DELETE CASCADE,
 	value TEXT NOT NULL,
 	view TEXT NOT NULL
@@ -328,6 +351,7 @@ COMMENT ON COLUMN project.value_view.view IS 'View.';
 
 CREATE TABLE project.translation (
 	id BIGSERIAL PRIMARY KEY,
+	space_id UUID NOT NULL,
 	project_id UUID NOT NULL REFERENCES project.project (id) ON DELETE CASCADE,
 	lang VARCHAR(64) NOT NULL,
 	key VARCHAR(64) NOT NULL,
