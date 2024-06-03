3615e5c23677985972492a7146395e2fbc0b0834
prt0117 <cpd900@yandex.ru>
2024-06-03 09:35:54 +0500

    Правки после запрошенных изменений в ПР

--- src/back/space/src/api/introspect.cpp
+++ src/back/space/src/api/introspect.cpp
@@ -37,38 +37,33 @@ std::string Introspect::HandleRequestThrow(
 		if (req.HasCookie(cookieName)) {
 			// извлекаем токен из куки
 			const std::string token = req.GetCookie(cookieName);
-			LOG_DEBUG() << "Cookie exists, token: " << token;
 
 			try {
 				// проверяем и распарсиваем токен
 				SpaceTokenPayload data = _s.Tokens().Verify(token);
-				LOG_DEBUG() << "Token verify OK";
 
 				// авторизуем и отдаём заголовки
 				req.GetHttpResponse().SetHeader(headers::kSpaceId, data._id);
 				req.GetHttpResponse().SetHeader(headers::kSpaceRole, data._role);
-				return "Ok";
+				req.SetResponseStatus(server::http::HttpStatus::kNoContent);
+				return {};
 			} catch(const std::exception& e) {
 				LOG_DEBUG() << "Fail to verify token: " << e.what();
 			}
 		}
 
-		LOG_DEBUG() << "Creating new token";
 		// получаем userId
 		if (!req.HasHeader(headers::kUserId))
 			throw errors::Unauthorized401();
 		std::string userId = req.GetHeader(headers::kUserId);
 
 		// Проверяем права и создаём токен
-		LOG_DEBUG() << "Checking rights";
 		model::Space space = _s.GetByKeyIfAdmin(spaceKey, userId);
 		const std::string spaceIdStr = boost::uuids::to_string(space.id);
 		const std::string role = "admin";
 		const std::string token = _s.CreateToken(spaceIdStr, space.key, userId, role);
-		LOG_DEBUG() << "Token created: " << token;
 
 		// Создаём куки
-		LOG_DEBUG() << "Creating cookie";
 		server::http::Cookie cookie{cookieName, token};
 		// todo - какие тут нам нужны настройки у создаваемой куки?
 		cookie.SetPath("/");
@@ -77,18 +72,18 @@ std::string Introspect::HandleRequestThrow(
 		cookie.SetSameSite("Lax");
 
 		// Возвращаем куки и заголовки в запросе
-		LOG_DEBUG() << "Setting Cookie and headers and returning OK";
 		auto& resp = req.GetHttpResponse();
 		resp.SetCookie(cookie);
 		req.GetHttpResponse().SetHeader(headers::kSpaceId, spaceIdStr);
 		req.GetHttpResponse().SetHeader(headers::kSpaceRole, role);
+		req.SetResponseStatus(server::http::HttpStatus::kNoContent);
 	}
 	catch(const std::exception& e) {
 		LOG_WARNING() << "Fail to verify: " << e.what();
 		req.SetResponseStatus(server::http::HttpStatus::kUnauthorized);
 		return "Invalid space authorization token";
 	}
-	return "Ok";
+	return {};
 }
 
 } // namespace svetit::space::handlers
