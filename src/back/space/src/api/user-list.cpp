1d41bf2051b9d8c0554ddb696caf10bb8941dac7
prt0117 <cpd900@yandex.ru>
2024-06-06 16:18:05 +0500

    ещё одна ручка с валидацией по схеме

--- src/back/space/src/api/user-list.cpp
+++ src/back/space/src/api/user-list.cpp
@@ -7,6 +7,7 @@
 #include <shared/parse/request.hpp>
 #include <shared/paging_serialize.hpp>
 #include <shared/schemas.hpp>
+#include <shared/parse/uuid.hpp>
 
 namespace svetit::space::handlers {
 
@@ -26,12 +27,14 @@ formats::json::Value UserList::HandleRequestJsonThrow(
 	formats::json::ValueBuilder res;
 
 	try {
-		const auto userId = req.GetHeader(headers::kUserId);
-		if (userId.empty())
-			throw errors::Unauthorized401{};
+		const auto params = ValidateRequest(_mapHttpMethodToSchema, req, body);
+		const auto userId = params[headers::kUserId].As<std::string>();
 
-		const auto spaceId = parseUUID(req, "spaceId");
-		const auto paging = parsePaging(req);
+		const auto spaceId = params["spaceId"].As<boost::uuids::uuid>();
+		Paging paging = {
+			.start = params["start"].As<int>(),
+			.limit = params["limit"].As<int>()
+		};
 		if (_s.IsListLimit(paging.limit))
 			throw errors::BadRequest400("Too big limit param");
 		res = _s.GetUserList(userId, spaceId, paging.start, paging.limit);
