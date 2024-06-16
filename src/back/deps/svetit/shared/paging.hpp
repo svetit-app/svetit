371071e20b3510839945196f19a854524f985b1c
Кирилл Зимников <kirill@zimnikov.ru>
2024-06-16 23:34:04 +0700

    170 пагинация за один запрос

--- src/back/deps/svetit/shared/paging.hpp
+++ src/back/deps/svetit/shared/paging.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
-#include "userver/formats/json/value.hpp"
+#include <shared/move_tuple.hpp>
+
+#include <userver/formats/json/value.hpp>
 #include <userver/server/handlers/http_handler_json_base.hpp>
 #include <userver/utest/using_namespace_userver.hpp>
+
+#include <boost/pfr/core.hpp>
+#include <boost/pfr/traits.hpp>
 
 namespace svetit {
 
@@ -11,7 +16,28 @@ struct Paging {
 };
 
 template<typename T>
-struct PagingResult {
+class PagingResult {
+	constexpr static auto makeTupleOfModelAndTotal() {
+		return std::tuple_cat(boost::pfr::structure_to_tuple(T{}), std::tuple<int64_t>{});
+	}
+
+public:
+	using RawContainer = std::vector<decltype(makeTupleOfModelAndTotal())>;
+
+	void operator=(RawContainer&& data)
+	{
+		items.reserve(data.size());
+		for (auto&& it : data) {
+			T item;
+			auto itemRef = boost::pfr::structure_tie(item);
+			moveTuple(std::move(it), itemRef);
+			items.push_back(std::move(item));
+		}
+
+		if (!data.empty())
+			total = std::get<std::tuple_size_v<typename RawContainer::value_type> - 1>(data.front());
+	}
+
 	std::vector<T> items;
 	int64_t total = 0;
 };
