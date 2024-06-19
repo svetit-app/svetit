a3a2b09cca726e70c9e547384b36eb54ddb1fc24
prt0117 <cpd900@yandex.ru>
2024-06-19 16:44:17 +0500

    объединение разделенной доки и генерация схем для локальной разработки (надо ещё потестить)

--- pipeline/schemas/generate.sh
+++ pipeline/schemas/generate.sh
@@ -2,20 +2,27 @@
 
 SCRIPT_PATH=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 
-DST_PATH="$SCRIPT_PATH/../../schemas"
-DST_HASH_PATH="$DST_PATH/.hash"
-DST_HASH=$(cat "$DST_HASH_PATH" 2>/dev/null)
-[ -d "$DST_PATH" ] || mkdir -p "$DST_PATH"
+SPLITTED_DIR_PATH="$SCRIPT_PATH/../../doc/api/splitted"
+SPLITTED_SAVED_HASH_PATH="$SPLITTED_DIR_PATH/../.hash"
+SPLITTED_SAVED_HASH=$(cat "$SPLITTED_SAVED_HASH_PATH" 2>/dev/null)
 
-SRC_PATH="$SCRIPT_PATH/../../doc/api/api.yaml"
-SRC_HASH=$(sha1sum "$SRC_PATH" | awk '{print $1}')
+GENERATED_HASH=$(find "$SPLITTED_DIR_PATH" -type f | xargs -I "{}" sha1sum "{}" | sha1sum | head -c 40)
 
-[ "$SRC_HASH" = "$DST_HASH" ] && exit 0
+[ "$GENERATED_HASH" = "$SPLITTED_SAVED_HASH" ] && exit 0
 
-rm -fr "$DST_PATH/*"
+docker run --rm -v "$SPLITTED_DIR_PATH":/spec redocly/cli bundle -o api-bundled.yaml ./openapi.yaml
+
+API_YAML_PATH="$SCRIPT_PATH/../../doc/api/api.yaml"
+
+mv -f "$SPLITTED_DIR_PATH"/api-bundled.yaml "$API_YAML_PATH"
+
+SCHEMAS_PATH="$SCRIPT_PATH/../../schemas"
+[ -d "$SCHEMAS_PATH" ] || mkdir -p "$SCHEMAS_PATH"
+
+rm -fr "$SCHEMAS_PATH/*"
 
 source "$SCRIPT_PATH/../venv/bin/activate"
-swagger2jsonschema --include-bodies --include-parameters "$SRC_PATH"
+swagger2jsonschema --include-bodies --include-parameters "$API_YAML_PATH"
 [ $? -ne 0 ] && exit $?
 
-echo -n "$SRC_HASH" > "$DST_HASH_PATH"
+echo -n "$GENERATED_HASH" > "$SPLITTED_SAVED_HASH_PATH"
