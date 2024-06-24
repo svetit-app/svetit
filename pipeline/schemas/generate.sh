65bff126dbc362b1207d8da8035cd40fdea63bbd
prt0117 <cpd900@yandex.ru>
2024-06-24 09:29:21 +0500

    перенёс api на уровень выше, доработал скрипт генерации для локальной разработки

--- pipeline/schemas/generate.sh
+++ pipeline/schemas/generate.sh
@@ -2,19 +2,20 @@
 
 SCRIPT_PATH=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 
-SPLITTED_DIR_PATH="$SCRIPT_PATH/../../doc/api/splitted"
-SPLITTED_SAVED_HASH_PATH="$SPLITTED_DIR_PATH/../.hash"
+SPLITTED_DIR_PATH="$SCRIPT_PATH/../../doc/api"
+SPLITTED_SAVED_HASH_PATH="$SPLITTED_DIR_PATH/.hash"
 SPLITTED_SAVED_HASH=$(cat "$SPLITTED_SAVED_HASH_PATH" 2>/dev/null)
 
-GENERATED_HASH=$(find "$SPLITTED_DIR_PATH" -type f | xargs -I "{}" sha1sum "{}" | sha1sum | head -c 40)
+GENERATED_HASH=$(find "$SPLITTED_DIR_PATH" -type f ! -name '.hash' | xargs -I "{}" sha1sum "{}" | sha1sum | head -c 40)
 
 [ "$GENERATED_HASH" = "$SPLITTED_SAVED_HASH" ] && exit 0
 
-docker run --rm -v "$SPLITTED_DIR_PATH":/spec redocly/cli bundle -o api-bundled.yaml ./openapi.yaml
+API_YAML_PATH_HOST="/tmp/svetit/api.yaml"
+API_YAML_PATH_GUEST="/tmp/api.yaml"
+TMP_PATH="/tmp/svetit/"
+[ -d "$TMP_PATH" ] || mkdir -p "$TMP_PATH"
 
-API_YAML_PATH="$SCRIPT_PATH/../../doc/api/api.yaml"
-
-mv -f "$SPLITTED_DIR_PATH"/api-bundled.yaml "$API_YAML_PATH"
+docker run --rm -v "$SPLITTED_DIR_PATH":/spec -v "$TMP_PATH":/tmp redocly/cli bundle -o "$API_YAML_PATH_GUEST" ./openapi.yaml
 
 SCHEMAS_PATH="$SCRIPT_PATH/../../schemas"
 [ -d "$SCHEMAS_PATH" ] || mkdir -p "$SCHEMAS_PATH"
@@ -22,7 +23,12 @@ SCHEMAS_PATH="$SCRIPT_PATH/../../schemas"
 rm -fr "$SCHEMAS_PATH/*"
 
 source "$SCRIPT_PATH/../venv/bin/activate"
-swagger2jsonschema --include-bodies --include-parameters "$API_YAML_PATH"
-[ $? -ne 0 ] && exit $?
+swagger2jsonschema --include-bodies --include-parameters "$API_YAML_PATH_HOST"
+[ $? -ne 0 ] && {
+	rm -f "$API_YAML_PATH_HOST";
+	exit $?;
+}
+
+rm -f "$API_YAML_PATH_HOST"
 
 echo -n "$GENERATED_HASH" > "$SPLITTED_SAVED_HASH_PATH"
