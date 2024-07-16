b05739284d01b546e6e5b8f55066fbf547544423
prt0117 <cpd900@yandex.ru>
2024-07-16 17:02:06 +0500

    WIP не до конца доделанное решение без отключения кеширования всей статики

--- src/web/src/app/app.module.ts
+++ src/web/src/app/app.module.ts
@@ -49,9 +49,10 @@ import { SpaceLinkListComponent } from './space/link-list/component';
 import { SpaceLinkJoinComponent } from './space/link-join/component';
 import { UserBadgeComponent } from './user-badge/component';
 import { UserBadgeService } from './user-badge/service';
+import { environment } from '../environments/environment';
 
 export function createTranslateLoader(http: HttpClient) {
-	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+	return new TranslateHttpLoader(http, './assets/i18n/', `.json?version=${environment.version}`);
 }
 
 @NgModule({
