bfea0d4cbf897fa2ef47934ed39e48d5e5260532
prt0117 <cpd900@yandex.ru>
2024-07-30 10:30:09 +0500

    изменил структуру основной таблицы

--- src/back/nodes/db/app/migrations/V0001__Init.sql
+++ src/back/nodes/db/app/migrations/V0001__Init.sql
@@ -6,14 +6,18 @@ CREATE TABLE nodes.nodes (
 	id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
 	name VARCHAR(64) NOT NULL,
 	description TEXT NOT NULL DEFAULT '',
-	geo POINT NOT NULL
+	latitude FLOAT NOT NULL DEFAULT 0.0::double precision,
+	longitude FLOAT NOT NULL DEFAULT 0.0::double precision,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 COMMENT ON TABLE nodes.nodes IS 'Nodes.';
 COMMENT ON COLUMN nodes.nodes.id IS 'Primary key of Node.';
 COMMENT ON COLUMN nodes.nodes.name IS 'Name of Node.';
 COMMENT ON COLUMN nodes.nodes.description IS 'Text description of Node.';
-COMMENT ON COLUMN nodes.nodes.geo IS 'Geographical coordinates of Node.';
+COMMENT ON COLUMN nodes.nodes.latitude IS 'Geographical latitude of Node.';
+COMMENT ON COLUMN nodes.nodes.longitude IS 'Geographical longitude of Node.';
+COMMENT ON COLUMN nodes.nodes.created_at IS 'Timestamp of Node creation.';
 
 CREATE TABLE nodes.node_project (
 	node_id UUID NOT NULL REFERENCES nodes.nodes (id) ON DELETE CASCADE,
