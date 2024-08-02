8fefb47f7f4c60b567445593a78f62f97f02975b
prt0117 <cpd900@yandex.ru>
2024-08-02 10:34:40 +0500

    завершение make run-% при ошибке загрузки моков

--- pipeline/run_with_mock.sh
+++ pipeline/run_with_mock.sh
@@ -8,7 +8,12 @@ make -C "$SCRIPT_PATH/../src/back/$1" service-start-debug &
 
 sleep 3
 
-psql -h localhost -p 15434 -U testsuite -d app_app < "$SCRIPT_PATH/../src/back/$1/tests/static/test_data.sql" || true
+psql --set ON_ERROR_STOP=on -h localhost -p 15434 -U testsuite -d app_app < "$SCRIPT_PATH/../src/back/$1/tests/static/test_data.sql"
+
+if [ $? -ne 0 ]; then
+	kill %1
+	exit 1
+fi
 
 jobs
 
