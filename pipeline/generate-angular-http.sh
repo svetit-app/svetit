50822c17c37a413f216029e623daad16d5032d0e
prt0117 <cpd900@yandex.ru>
2024-08-13 10:46:36 +0500

    скрипт удаления хедеров, генерируемых на лету, из доки перед генерацией http клиента

--- pipeline/generate-angular-http.sh
+++ pipeline/generate-angular-http.sh
@@ -2,21 +2,43 @@
 
 SCRIPT_PATH=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 
-PROJECT_ROOT_PATH="$SCRIPT_PATH/../"
-DOCKER_OPENAPI_PATH="/local/doc/api/openapi.yaml"
+DOCKER_OPENAPI_PATH="/local/openapi.yaml"
 DOCKER_OUTPUT_PATH="/local/src/web/src/app/api/"
-PROJECT_GENERATED_PATH="$SCRIPT_PATH/../src/web/src/app/api"
+PROJECT_GENERATED_PATH="$SCRIPT_PATH/../src/web/src/app/api/"
+
+SPLITTED_DIR_PATH="$SCRIPT_PATH/../doc/api"
+TMP_PATH="/tmp/svetit/"
+TMP_PATH_OPENAPI_PATHS="/tmp/svetit/paths"
+
+[ -d "$TMP_PATH" ] || mkdir -p "$TMP_PATH"
+
+cp -r ${SPLITTED_DIR_PATH}/* ${TMP_PATH}
+
+for file in "$TMP_PATH_OPENAPI_PATHS"/*; do
+    if [ -f "$file" ]; then
+      echo "$file"
+      sed -i '/XUserHeader.yaml/d' "$file"
+      sed -i '/XSessionHeader.yaml/d' "$file"
+      sed -i '/XSpaceIdHeader.yaml/d' "$file"
+      sed -i '/XSpaceRoleHeader.yaml/d' "$file"
+      sed -i '/  parameters:/ { N; /  parameters:\n  responses:/ { s/  parameters:\n//; } }' "$file"
+      sed -i 's/handler-//g' "$file"
+    fi
+done
 
 docker run --rm --user "$(id -u):$(id -g)" \
-  -v ${PROJECT_ROOT_PATH}:/local openapitools/openapi-generator-cli generate \
+  -v ${TMP_PATH}:/local openapitools/openapi-generator-cli generate \
   -i ${DOCKER_OPENAPI_PATH} \
   -g typescript-angular \
   -o ${DOCKER_OUTPUT_PATH} \
   --additional-properties ngVersion=16.2.14,fileNaming=kebab-case,withInterfaces=true --generate-alias-as-model
+
+cp -r "${TMP_PATH}src/web/src/app/api/" ${PROJECT_GENERATED_PATH}
+
+#rm -rf ${TMP_PATH}
 
 rm -rf "$PROJECT_GENERATED_PATH/.openapi-generator"
 rm -rf "$PROJECT_GENERATED_PATH/.gitignore"
 rm -rf "$PROJECT_GENERATED_PATH/.openapi-generator-ignore"
 rm -rf "$PROJECT_GENERATED_PATH/git_push.sh"
 rm -rf "$PROJECT_GENERATED_PATH/README.md"
-rm -rf "$PROJECT_ROOT_PATH/openapitools.json"
